2c5f388c5989c3f1ade5de4533ee26e5b8bb4472
yuvalnv@gmail.com <yuvalnv@gmail.com@3a3e0960-3617-316a-a124-8a4adbc1bdb2>
2011-12-09 22:32:31 +0000

    Added the LED library (originally from Wiring) Adopted to work on Arduino, added default constructor (parameterless, works on default Arudino LED port)

---
+++ Arduino/Libraries/LED/LED.h
@@ -0,0 +1,52 @@
+/* $Id: LED.h 1198 2011-06-14 21:08:27Z bhagman $
+||
+|| @author         Alexander Brevig <abrevig@wiring.org.co>
+|| @url            http://wiring.org.co/
+|| @url            http://alexanderbrevig.com/
+|| @contribution   Brett Hagman <bhagman@wiring.org.co>
+||
+|| @description
+|| | This is a Hardware Abstraction Library for LEDs.
+|| | Provides an easy way of handling LEDs.
+|| |
+|| | Wiring Cross-platform Library
+|| #
+||
+|| @license Please see cores/Common/License.txt.
+||
+||
+|| Modified by Yuval Naveh 12/9/2001:
+||   1. Adapted to Arduino (Original Code from Wiring 1.00)
+||   2. Default constructor added (using On Board LED)
+*/
+
+#ifndef LED_H
+#define LED_H
+
+#include "WProgram.h"
+
+#define ARDUINO_ON_BOARD_LED_PIN 13
+
+class LED
+{
+  public:
+    LED();
+    LED(uint8_t ledPin);
+
+    bool getState();
+    void on();
+    void off();
+    void toggle();
+    void blink(unsigned int time, byte times = 1);
+
+    void setValue(byte val);
+    void fadeIn(unsigned int time);
+    void fadeOut(unsigned int time);
+
+  private:
+    bool status;
+    uint8_t pin;
+};
+
+#endif
+// LED_H

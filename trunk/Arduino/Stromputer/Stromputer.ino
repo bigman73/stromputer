dba144e64091bea2ce05c205b20df30917bc93e6
yuvalnv@gmail.com <yuvalnv@gmail.com@3a3e0960-3617-316a-a124-8a4adbc1bdb2>
2013-01-31 03:03:12 +0000

    License changed to Common Creative Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0), by-nc-sa

--- trunk/Arduino/Stromputer/Stromputer.ino
+++ trunk/Arduino/Stromputer/Stromputer.ino
@@ -14,6 +14,17 @@
     
     >> This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License: by-nc-sa <<    
     http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode
+
+    You are free:
+
+      to Share — to copy, distribute and transmit the work
+      to Remix — to adapt the work
+
+    Under the following conditions:
+
+      Attribution — You must attribute the work in the manner specified by the author or licensor (but not in any way that suggests that they endorse you or your use of the work).
+      Noncommercial — You may not use this work for commercial purposes.
+      Share Alike — If you alter, transform, or build upon this work, you may distribute the resulting work only under the same or similar license to this one.
 */
 
 /*

4aeffde09a0b29dbf48e135c0b1271e1f39ef8b2
yuvalnv@gmail.com <yuvalnv@gmail.com@3a3e0960-3617-316a-a124-8a4adbc1bdb2>
2013-01-31 02:53:18 +0000

    License changed to Common Creative Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0), by-nc-sa

--- Arduino/Stromputer/Stromputer.ino
+++ Arduino/Stromputer/Stromputer.ino
@@ -12,19 +12,9 @@
 /*
     Stromputer - Enhanced display for Suzuki V-Strom Motorcycles (DL-650, DL-1000, years 2004-2011)
     Copyright (C) 2011 Yuval Naveh
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    >> This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License: by-nc-sa <<    
+    http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode
 */
 
 /*
